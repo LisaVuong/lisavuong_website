ceef633490ea596f525e26592b89ebf2ac175713
Achal Varma <achalv2.0@gmail.com>
2014-10-29 23:47:07 -0500

    ship ship ship :ship:

--- js/all.js
+++ js/all.js
@@ -3,69 +3,73 @@ $(document).ready(function(){
 	var _container 		= $('.container');
 	var _portfolioblock	= $('.item-image');
 	var _foldcontent	= $('.fold-content');
-	var _bannerimg		= $('.banner-img');
+	var _tapestryimg	= $('.tapestry-img');
 
 	//touch support?
 	document.addEventListener("touchstart", function(){}, true)
 	
-	function resizeBanners() {
+	function resizetapestries() {
 		var _windowwidth 	= $(window).width();
-		var _bannerwidth 	= _bannerimg.width();
+		var _tapestrywidth 	= _tapestryimg.width();
 		var _widthdiff		= 0;
-		if (_windowwidth < _bannerwidth) {
-			_widthdiff = _bannerwidth - _windowwidth;
+		if (_windowwidth < _tapestrywidth) {
+			_widthdiff = _tapestrywidth - _windowwidth;
 			console.log(_widthdiff);
 			_widthdiff = (_widthdiff/2);
 			_widthdiff = 0 - _widthdiff;
-			_bannerimg.css("margin-left", _widthdiff);
+			_tapestryimg.css("margin-left", _widthdiff);
 		}
-		else if(_bannerwidth < _windowwidth) {
-			_bannerimg.css("margin-left", 0);
+		else if(_tapestrywidth < _windowwidth) {
+			_tapestryimg.css("margin-left", 0);
 		}
 		else {
 			console.log("nah");
 		}
 	}
 
-	resizeBanners();
+	resizetapestries();
 
 	$(window).resize(function() {
-		resizeBanners();
+		resizetapestries();
 	});
 
     $(function() {
-        var num = _bannerimg.length;
-        _bannerimg.each(function(i) {
+        var num = _tapestryimg.length;
+        _tapestryimg.each(function(i) {
             $(this).delay((i++)*100)
-            .velocity({opacity: 1}, 150)
+            .velocity({opacity: 1}, 300)
             .animate({filter: "greyscale(0)"}, 300);
-            // .velocity({opacity: 0.2}, 250)
-            // .velocity({opacity: 1}, 250)
         });
     });
 
-		_portfolioblock.click(function() {
-			var _clickindex = _portfolioblock.index(this);
-			console.log(_clickindex);
-	    	if($(this).next().hasClass("unfolded")){
-	    		$(this).next().slideUp();
-	    		$(this).next().removeClass("unfolded");
-    		$(this).find("img.banner-img").addClass("b_w");
-	    	}
-	    	else {
-			    _foldcontent.slideUp(300);
-				_foldcontent.removeClass("unfolded");
-			    $(this).next().addClass("unfolded");
-		    $(this).find("img.banner-img").removeClass("b_w");
-			    $(this).next().slideDown(200, function(){
-			    	var that = $(this);
-			    	$("html, body").animate({
-			    		scrollTop: that.offset().top-150
-			    	},300);
-			    });
+	_portfolioblock.click(function() {
+		var _clickindex = _portfolioblock.index(this);
+    	if($(this).next().hasClass("unfolded")) {
+    		$(this).next().slideUp();
+    		$(this).next().removeClass("unfolded");
+			$(this).find("img.tapestry-img").addClass("b_w");
+			if(_clickindex===0) {
+				$("html, body").animate({
+		    		scrollTop: $("html, body").offset().top
+		    	}, 300);
 			}
-		    return false;
-		});		
+    	}
+    	else {
+		    _foldcontent.slideUp(300);
+			_foldcontent.removeClass("unfolded");
+		    $(this).next().addClass("unfolded");
+	    	$(this).find("img.tapestry-img").removeClass("b_w");
+		    $(this).next().slideDown(200, function(){
+		    	var that = $(this);
+		    	setTimeout(function() {
+			    	$("html, body").animate({
+				   		scrollTop: that.position().top-150
+				    },300);
+				}, 100);
+		    });
+		}
+	    return false;
+	});		
     
 	$('.slider').cbpFWSlider();
 });
